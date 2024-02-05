04ae62534fc152e5a29cedc5e06025feaed3b4fe
Dakota <dakotacustompcs@gmail.com>
2024-02-04 20:15:15 -0500

    Creating objects and symbols.

--- home.js
+++ home.js
@@ -1,1 +1,13 @@
-changePercentOff( 32 );
+
+let mySymbol = Symbol();
+
+let person = {
+  name: 'John',
+  age: 32,
+  partTime: false,
+  [mySymbol]: 'Secret Information'
+};
+
+person['age'] = 44;
+
+showMessage(person.age);
